dee40304332c4cd5edb7d2000e89001c36ca2cc4
Edgar Munoz Q <edgmuo1085@gmail.com>
2024-12-10 23:04:12 -0500

    se agrega componentes para todos los proyectos

--- projects/mf-shell/src/app/app.component.ts
+++ projects/mf-shell/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnDestroy } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { CommonsLibService } from '@commons-lib';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,33 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'mf-shell';
+  countCart: number = 0;
+  suscribe$!: Subscription;
+
+  constructor(
+    private router: Router,
+    public commonsLibService: CommonsLibService
+  ) {
+    this.suscribe$ = this.commonsLibService.channelPayment$.subscribe({
+      next: response => {
+        this.countCart = response;
+      },
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.suscribe$) {
+      this.suscribe$.unsubscribe();
+    }
+  }
+
+  clicCart() {
+    this.router.navigate(['/payment']);
+  }
+
+  clicHome() {
+    this.router.navigate(['/']);
+  }
 }
